298567c09a7983add03a27c17853905e94c2f816
Yury Semikhatsky <yurys@chromium.org>
2020-09-24 10:41:00 -0700

    Remove <p>

--- java/src/main/java/com/microsoft/playwright/tools/ApiGenerator.java
+++ java/src/main/java/com/microsoft/playwright/tools/ApiGenerator.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) Microsoft Corporation.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,6 @@ import com.google.gson.JsonObject;
 
 import java.io.*;
 import java.util.*;
-import java.util.function.BiConsumer;
 
 public class ApiGenerator {
   private List<String> output;
@@ -38,13 +37,13 @@ public class ApiGenerator {
 
   private static String header = "/**\n" +
     " * Copyright (c) Microsoft Corporation.\n" +
-    " * <p>\n" +
+    " *\n" +
     " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
     " * you may not use this file except in compliance with the License.\n" +
     " * You may obtain a copy of the License at\n" +
-    " * <p>\n" +
+    " *\n" +
     " * http://www.apache.org/licenses/LICENSE-2.0\n" +
-    " * <p>\n" +
+    " *\n" +
     " * Unless required by applicable law or agreed to in writing, software\n" +
     " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
     " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
