f23aa44900e3fbc5732e98351bb6bcd88c572d2b
Yury Semikhatsky <yurys@chromium.org>
2022-09-16 18:42:37 -0700

    examples

--- packages/playwright-test/src/gherkinLoader.ts
+++ packages/playwright-test/src/gherkinLoader.ts
@@ -15,7 +15,7 @@
  */
 
 import { AstBuilder, GherkinClassicTokenMatcher, Parser, compile } from '@cucumber/gherkin';
-import type { Scenario } from '@cucumber/messages';
+import type { Scenario, Step } from '@cucumber/messages';
 import { IdGenerator } from '@cucumber/messages';
 import * as fs from 'fs';
 import { Suite, TestCase } from './test';
@@ -38,12 +38,35 @@ export async function loadGherkinFeatureFile(parent: Suite, file: string, enviro
   // TODO: figure out how to get appropriate test type for the feature!
   const testType = TestTypeImpl._fromTest(test);
 
+
+  const backgroundSteps: Step[] = [];
+  for (const child of gherkinDocument.feature.children) {
+    if (child.background)
+      backgroundSteps.push(...child.background.steps);
+  }
+
   const addTestCaseForScenario = (scenario: Scenario) => {
     const parameterNames = new Set<string>();
+
+    const steps = [...backgroundSteps];
+
+    // if (scenario.examples.length) {
+    //   // scenario.
+    //   for (const example of scenario.examples) {
+    //     const names =  example.tableHeader?.cells.map(c => c.value);
+    //     for (const row of example.tableBody) {
+    //       // row.cells.map()
+    //     }
+    //   }
+    // } else {
+    steps.push(...scenario.steps);
+    // }
+
     // Derive all parameter names from step definitions.
-    for (const step of scenario.steps) {
+    for (const step of steps) {
       // TODO: use keywordType instead to support localization
       const definitions = Suite._globalBddSteps.filter(definition => {
+        // TODO: match And, But etc.
         if (definition.type !== step.keyword.trim())
           return;
         const mathes = definition.expression.match(step.text);
@@ -60,9 +83,9 @@ export async function loadGherkinFeatureFile(parent: Suite, file: string, enviro
     }
 
     const testFn = async (fixtures: any) => {
-      debugTest(`running steps for scenario: ${scenario.name}`);
-      for (const step of scenario.steps) {
-      // TODO: use keywordType instead to support localization
+      debugTest(`  bdd running steps for scenario: ${scenario.name}`);
+      for (const step of steps) {
+        // TODO: use keywordType instead to support localization
         const definitions = Suite._globalBddSteps.filter(definition => {
           if (definition.type !== step.keyword.trim())
             return;
@@ -76,9 +99,9 @@ export async function loadGherkinFeatureFile(parent: Suite, file: string, enviro
         const definition = definitions[0];
         const stepArguments = definition.expression.match(step.text)!.map(arg => arg.getValue({}));
         const fn = definition.fn;// Extract a variable to get a better stack trace ("myTest" vs "TestCase.myTest [as fn]").
-        debugTest(`bdd step started: ${step.text}`);
+        debugTest(`  bdd step started: ${step.text}`);
         await fn(fixtures, ...stepArguments);
-        debugTest(`bdd step finished: ${step.text}`);
+        debugTest(`  bdd step finished: ${step.text}`);
       }
     };
     setFixtureParameterNames(testFn, Array.from(parameterNames));
@@ -109,8 +132,8 @@ export async function loadGherkinDocument(file: string) {
   const parser = new Parser(builder, matcher);
   const content = (await fs.promises.readFile(file)).toString('utf-8');
   const gherkinDocument = parser.parse(content);
+  // console.log('*** gherkinDocument = ' + JSON.stringify(gherkinDocument, null, 2));
   return gherkinDocument;
-  //   console.log('*** gherkinDocument = ' + JSON.stringify(gherkinDocument, null, 2));
   //   const pickles = await compile(gherkinDocument, 'file:///home/yurys/sandbox/pw-bdd/tests/features/playwright.feature', uuidFn);
   //   console.log('\n\n\n');
 
