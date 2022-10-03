e0e7e15c662c2fb1bc75839b639018ae1e764686
Yury Semikhatsky <yurys@chromium.org>
2022-10-03 10:29:33 -0700

    New version, with spread opeator

--- groups/3/playwright.config.ts
+++ groups/3/playwright.config.ts
@@ -14,31 +14,66 @@
  * limitations under the License.
  */
 
- import type { PlaywrightTestConfig } from '@playwright/experimental-ct-react';
- import { devices } from '@playwright/experimental-ct-react';
- import { group } from 'console';
- 
- const config: PlaywrightTestConfig = {
-   testDir: 'src',
-   forbidOnly: !!process.env.CI,
-   retries: process.env.CI ? 2 : 0,
-   reporter: 'html',
-   use: {
-     ctPort: 3102,
-     trace: 'on-first-retry',
-   },
-   // Defaults:
-   //   stage: 0
-   //   runAlways: false
-   //   stopOnFailure: false
-   //
-   // runAlways is redundant - can be solved by running --project=setup,smoke,teardown instead
-   // stage aka order, phase, ...
-   // workers - later
-   projects: [
+import type { PlaywrightTestConfig } from '@playwright/experimental-ct-react';
+import { devices } from '@playwright/experimental-ct-react';
+import { group } from 'console';
+
+
+const stages = {
+  GLOBAL_SETUP: {
+    order: -20,
+    workers: 1,
+    stopOnFailures: true,
+  },
+  SMOKE_TESTS: {
+    order: -10,
+    fullyParallel: true,
+    stopOnFailures: true,
+  }
+};
+
+const setup = {
+  order: -10,
+  workers: 1,
+  stopOnFailures: true,
+};
+const smokeTests = {
+  order: -10,
+  workers: 1,
+  stopOnFailures: true,
+};
+
+const config: PlaywrightTestConfig = {
+  testDir: 'src',
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+  use: {
+    ctPort: 3102,
+    trace: 'on-first-retry',
+  },
+  // Defaults:
+  //   stage: 0
+  //   runAlways: false
+  //   stopOnFailure: false
+  //
+  // runAlways is redundant - can be solved by running --project=setup,smoke,teardown instead
+  // stage aka order, phase, ...
+  // workers - later, per project?
+  // 
+  projects: [
+    {
+      name: 'setup-login',
+      testMatch: '**/*setup-login.ts',
+      ...stages.GLOBAL_SETUP,
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
     {
       name: 'setup-db',
       testMatch: '**/*setup-db.ts',
+      ...stages.GLOBAL_SETUP,
       stage: -30,
       canShard: false,  // noshard, runInEachShard
       stopOnFailure: true,
@@ -46,18 +81,17 @@
         ...devices['Desktop Chrome'],
       },
     },
-
     {
-       name: 'setup',
-       testMatch: '**/*setup.ts',
-       stage: -20,
-       runAlways: true, // ==> shards = false, 
-       stopOnFailure: true,
-       use: {
-         ...devices['Desktop Chrome'],
-       },
-     },
-     {
+      name: 'setup',
+      testMatch: '**/*setup.ts',
+      stage: -20,
+      runAlways: true, // ==> shards = false, 
+      stopOnFailure: true,
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
       name: 'desktop chromium @smoke',
       stage: -10,
       stopOnFailure: true,
@@ -65,41 +99,54 @@
       use: {
         ...devices['Desktop Chrome'],
       },
-     },
-     {
-        name: 'desktop chromium',
-        grepInvert: /@smoke/,
-        use: {
-         ...devices['Desktop Chrome'],
-       },
-     },
-     {
-       name: 'mobile chromium',
-       grepInvert: /@smoke/,
-       stage: 20,
-       use: {
-         ...devices['Pixel 6'],
-       },
-     },
-     {
+    },
+    {
+      name: 'desktop chromium',
+      grepInvert: /@smoke/,
+      stage: 20,
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'mobile chromium',
+      grepInvert: /@smoke/,
+      stage: 20,
+      use: {
+        ...devices['Pixel 6'],
+      },
+    },
+    {
       name: 'desktop webkit',
       grepInvert: /@smoke/,
       use: {
         ...devices['Desktop WebKit'],
       },
-      {
-        name: 'teardown',
-        testMatch: '**/*teardown.ts',
-        stage: 100,
-        canShard: false,
-        runAlways: true,
-        use: {
-          ...devices['Desktop Chrome'],
-        },
+    },
+    {
+      name: 'teardown',
+      testMatch: '**/*teardown.ts',
+      stage: 100,
+      canShard: false,
+      runAlways: true,
+      // runAfterFailuresToo: true,
+      use: {
+        ...devices['Desktop Chrome'],
       },
     },
- ],
- };
- 
- export default config;
- 
+  ],
+  stages: {
+    'setup': {
+      order: -10,
+      workers: 1,
+      stopOnFailures: true,
+    },
+    'smokeTests': {
+      order: -10,
+      workers: 1,
+      stopOnFailures: true,
+    }
+  }
+};
+
+export default config;
